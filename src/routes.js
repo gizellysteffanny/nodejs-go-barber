1412caee0955a65e42a73b9fa8b44c24eea7533b
Gizelly <gizellysteffanny@gmail.com>
2020-01-26 16:14:31 -0400

    editorConfig, setup eslint and prettier

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Welcome to the barber!" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Welcome to the barber!' });
 });
 
 export default routes;

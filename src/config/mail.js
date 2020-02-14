67b0d382154f298dec0ea05e8f1b9f8e284158c7
Gizelly <gizellysteffanny@gmail.com>
2020-02-13 23:17:19 -0400

    config and send mail with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: '',
+  auth: {
+    user: 'f2c0ebcbaec159',
+    pass: 'db54018589c3bc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
